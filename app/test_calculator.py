a9ff53e4a15772d57525326aa3cb8515f9c4dc1e
Jiwon Jeon <jiwonjeonn@gmail.com>
2024-03-06 18:33:54 +0900

    decrease
    Signed-off-by: Jiwon Jeon <jiwonjeonn@gmail.com>

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -7,10 +7,10 @@ def test_add():
     assert Calculator.add(0, 2.0) == 2.0
     assert Calculator.add(2.0, 0) == 2.0
     assert Calculator.add(-4, 2.0) == -2.0
-def test_subtract():
-    assert Calculator.subtract(1, 2) == -1.0
-    assert Calculator.subtract(2, 1) == 1.0
-    assert Calculator.subtract(1.0, 2.0) == -1.0
-    assert Calculator.subtract(0, 2.0) == -2.0
-    assert Calculator.subtract(2.0, 0.0) == 2.0
-    assert Calculator.subtract(-4, 2.0) == -6.0
+# def test_subtract():
+#     assert Calculator.subtract(1, 2) == -1.0
+#     assert Calculator.subtract(2, 1) == 1.0
+#     assert Calculator.subtract(1.0, 2.0) == -1.0
+#     assert Calculator.subtract(0, 2.0) == -2.0
+#     assert Calculator.subtract(2.0, 0.0) == 2.0
+#     assert Calculator.subtract(-4, 2.0) == -6.0
